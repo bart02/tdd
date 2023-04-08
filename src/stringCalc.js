3f2f789a12958c6c406c3d80acad44ae9ec03484
Artem Batalov <a@batalov.me>
2023-04-08 11:34:18 +0300

    5 wave

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,11 +1,11 @@
 const calc = (input) => {
     let delimiter = /\n|,/u
-    if (input.startsWith('//'))
+    if (input.startsWith('//')) {
         [, delimiter] = input.match(/\/\/(.*)\n/u);
         input = input.replace(/\/\/(.*)\n/u, '');
-    if (input === '') {
-        return 0;
     }
+    
+    if (input === '') return 0;
     return input.split(delimiter).reduce((acc, curr) => {
         return acc + parseInt(curr, 10);
     }, 0);
