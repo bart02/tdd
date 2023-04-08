9e3b891986403ad62a910ef009bc215b3dc7334c
Artem Batalov <a@batalov.me>
2023-04-08 11:17:33 +0300

    2 wawe

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,3 +1,10 @@
-const calc = () => 0;
+const calc = (input) => {
+    if (input === '') {
+        return 0;
+    }
+    return input.split(',').reduce((acc, curr) => {
+        return acc + parseInt(curr, 10);
+    }, 0);
+};
 
 export default calc;
